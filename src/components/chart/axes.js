1a4bd9c52f3633212c23167539e26394a071f6ca
MuyMalo <alexjmalo@gmail.com>
2019-12-09 15:35:11 -0600

    added key to rows to get rid of error

--- src/components/chart/axes.js
+++ src/components/chart/axes.js
@@ -2,24 +2,28 @@ import React from 'react';
 import './axes.css';
 
 const Axes = (props) => {
+
+  
     
     var dataColumns = props.data.columns;
     var dataRows = props.data.rows;
 
-    var tableBody = dataRows.map(function(row) {
+    //was getting error that each child didn't have a key...
+    //added i as second parameter in function to give
+    //key to each row of table, not sure why it works
+    //but error is gone
+    var tableBody = dataRows.map(function(row, i) {
       return (
-        //was getting validateDOMNesting error without
-        //tbody, tr cannot be child of table
-        <tbody>
-          <tr>
-            {dataColumns.map(function(column) {
-              return <td></td>; })}
+          <tr key={i}>
+            {dataColumns.map(function(column, i) {
+              return <td key={i++}></td>; })}
           </tr>
-        </tbody>); 
+        ); 
     });
-     
+    //was getting validateDOMNesting error without
+    //tbody, tr cannot be child of table
     return (<table className="table">
-              {tableBody}
+              <tbody>{tableBody}</tbody>
             </table>) 
 };
 
