7b693dfe5f1d1122c74aeb1105f19bb53556646a
MuyMalo <alexjmalo@gmail.com>
2019-12-10 23:02:04 -0600

    pushing changes made today

--- src/App.js
+++ src/App.js
@@ -3,22 +3,9 @@ import React from 'react';
 import CandlestickChart from './components/chart/candlestick-chart.js';
 
 function App() {
-  let url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
-
-  let jsonData;    
-  fetch(url).then(
-    function(u){ return u.json();}
-  ).then(
-    function(json){
-      jsonData = json;
-    }
-  )
-
-  console.log(JSON.stringify(jsonData));
 
   return (
     <div className="App">
-      <CandlestickChart />
       <CandlestickChart />
     </div>
   );
--- src/components/chart/candlestick-chart.js
+++ src/components/chart/candlestick-chart.js
@@ -4,6 +4,25 @@ import Axes from './axes';
 
 const CandlestickChart = (props) => {
     
+
+  let url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
+
+  var jsonData;
+
+  fetch(url).then(
+    function(u){ return u.json();}
+  ).then(
+    function(json){
+      //jsonData = json["Meta Data"]["1. Information"];      
+      jsonData = json;
+      console.log(jsonData);
+    }
+  )
+  //console.log(jsonData) logs "undefined" when used outside 
+  //of above function because it's called before the above 
+  //function gets response from api... fetch is asynchronous
+  //need callback function
+  
     var tableData = {
         columns: ['Service', '', 'Unit', '', '', '', '', '', '', ''],
         rows: [{
