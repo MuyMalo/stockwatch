18cd95a1ea4a3857d3ade0aecd7f7f64930fd559
MuyMalo <alexjmalo@gmail.com>
2019-12-18 22:55:43 -0600

    found useFetch hook on SO

--- src/components/chart/axes.js
+++ src/components/chart/axes.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './axes.css';
 
+const useFetch = (url) => {
+  const [data, updateData] = useState(undefined);
+
+  // empty array as second argument equivalent to componentDidMount
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(url);
+      const json = await response.json();
+      updateData(json);
+    }
+    fetchData();
+  }, [url]);
+
+  return data;
+};
+
 const Axes = (props) => {
-
   
-    
-    var dataColumns = props.data.columns;
-    var dataRows = props.data.rows;
+  const data = useFetch(props.data);
 
-    //was getting error that each child didn't have a key...
-    //added i as second parameter in function to give
-    //key to each row of table, not sure why it works
-    //but error is gone
-    var tableBody = dataRows.map(function(row, i) {
-      return (
-          <tr key={i}>
-            {dataColumns.map(function(column, i) {
-              return <td key={i++}></td>; })}
-          </tr>
-        ); 
-    });
-    //was getting validateDOMNesting error without
-    //tbody, tr cannot be child of table
-    return (<table className="table">
-              <tbody>{tableBody}</tbody>
-            </table>) 
+  var dataColumns = data["Time Series (Daily)"];
+  var dataRows = props.data.rows;
+
+  var tableBody = dataRows.map(function(row, i) {
+    return (
+      <tr key={i}>
+        {dataColumns.map(function(column) {
+          return <td></td>; })}
+      </tr>
+    ); 
+  });
+    
+  return (<table className="table">
+            <tbody>{tableBody}</tbody>
+          </table>) 
 };
 
 export default Axes;
