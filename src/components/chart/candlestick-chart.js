7dfb682627e77b435eb3e3c5a52fd2d919acec2e
MuyMalo <alexjmalo@gmail.com>
2019-12-18 22:56:10 -0600

    got rid of unnecessary code

--- src/components/chart/candlestick-chart.js
+++ src/components/chart/candlestick-chart.js
@@ -4,80 +4,16 @@ import Axes from './axes';
 
 const CandlestickChart = (props) => {
     
-
   let url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 
-  var jsonData = fetch(url).then(
-    function(res){ return res.json();}
-  )
-  
-  //outputs a promise object?
-  console.log(jsonData);
-  
-    var tableData = {
-        columns: ['Service', '', 'Unit', '', '', '', '', '', '', ''],
-        rows: [{
-          'Service': 'Veterinary Assitance',
-          'Cost/Unit': 50,
-          'Unit': '1 Hour',
-          'Units Requested': 12
-        }, {
-          'Service': 'Veterinary Assitance',
-          'Cost/Unit': 50,
-          'Unit': '1 Hour',
-          'Units Requested': 12
-        }, {
-          'Service': 'Veterinary Assitance',
-          'Cost/Unit': 50,
-          'Unit': '1 Hour',
-          'Units Requested': 12
-        }, {
-          'Service': 'Veterinary Assitance',
-          'Cost/Unit': 50,
-          'Unit': '1 Hour',
-          'Units Requested': 12
-        }, {
-          'Service': 'Veterinary Assitance',
-          'Cost/Unit': 50,
-          'Unit': '1 Hour',
-          'Units Requested': 12
-        }, {
-          'Service': 'Veterinary Assitance',
-          'Cost/Unit': 50,
-          'Unit': '1 Hour',
-          'Units Requested': 12
-        }, {
-          'Service': 'Veterinary Assitance',
-          'Cost/Unit': 50,
-          'Unit': '1 Hour',
-          'Units Requested': 12
-        }, {
-          'Service': 'Veterinary Assitance',
-          'Cost/Unit': 50,
-          'Unit': '1 Hour',
-          'Units Requested': 12
-        }, {
-          'Service': 'Veterinary Assitance',
-          'Cost/Unit': 50,
-          'Unit': '1 Hour',
-          'Units Requested': 12
-        }, {
-          'Service': 'foo',
-          'Unit': null,
-          'Cost/Unit': undefined,
-          'Units Requested': 42
-        }]
-      };
-
-      return (
-        <div className="container">
-            <Axes data={tableData} />
-            <div className="candlesticks">
-                
-            </div>
-
+  return (
+    <div className="container">
+        <Axes data={url} />
+        <div className="candlesticks">
+          
         </div>
-    )
+    </div>
+  )
 }
 
 export default CandlestickChart;
